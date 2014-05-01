977c471b78bcb05263accb523cce2fa9242fff80
Sidhant Godiwala <grinnbearit@liara.local>
2014-05-01 19:19:33 +0530

    show/hide images

--- src/client/triple_triad/core.cljs
+++ src/client/triple_triad/core.cljs
@@ -26,37 +26,55 @@
 
 
 (defn card-view
-  [app owner opts]
-  (om/component
-   (html [:tr {:key (:name opts)}
-          [:td
-           [:div (:name opts)]
-           [:img {:src (:file opts)}]]
-          [:td (:level opts)]
-          [:td (format-rank (:top opts))]
-          [:td (format-rank (:right opts))]
-          [:td (format-rank (:bottom opts))]
-          [:td (format-rank (:left opts))]
-          [:td (format-element (:element opts))]
-          [:td (:location opts)]])))
+  [app owner {:keys [card]}]
+  (reify
+    om/IRenderState
+    (render-state [_ {:keys [image?]}]
+      (html [:tr {:key (:name card)}
+             [:td
+              [:div (:name card)]
+              (when image?
+                [:img {:src (:file card)}])]
+             [:td (:level card)]
+             [:td (format-rank (:top card))]
+             [:td (format-rank (:right card))]
+             [:td (format-rank (:bottom card))]
+             [:td (format-rank (:left card))]
+             [:td (format-element (:element card))]
+             [:td (:location card)]]))))
 
 
 (defn card-list
-  [app]
+  [app owner]
   (reify
+    om/IInitState
+    (init-state [_]
+      {:images? true})
+
     om/IWillMount
     (will-mount [_]
       (go (let [cards (<! (api/fetch-cards))]
             (om/update! app [:cards] cards))))
-    om/IRender
-    (render [_]
+
+    om/IRenderState
+    (render-state [_ {:keys [images?]}]
       (html [:table
              [:thead
               [:tr
-               [:th "name"] [:th "level"] [:th "top"] [:th "right"]
-               [:th "bottom"] [:th "left"] [:th "element"] [:th "location"]]]
+               [:th
+                [:button {:on-click #(om/update-state! owner :images? not)}
+                 (if images? "-" "+")]
+                [:span "name"]]
+               [:th "level"]
+               [:th "top"]
+               [:th "right"]
+               [:th "bottom"]
+               [:th "left"]
+               [:th "element"]
+               [:th "location"]]]
              [:tbody (for [card (:cards app)]
-                       (om/build card-view app {:opts card}))]]))))
+                       (om/build card-view app {:opts {:card card}
+                                                :state {:image? images?}}))]]))))
 
 
 (om/root card-list app-state {:target (. js/document (getElementById "app"))})
