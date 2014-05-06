6249380df62b21e5f6a244ba8d1cf6f9a100344a
Sidhant Godiwala <grinnbearit@liara.local>
2014-05-06 15:18:15 +0530

    list hands

--- src/client/triple_triad/board.cljs
+++ src/client/triple_triad/board.cljs
@@ -1,15 +1,53 @@
 (ns triple-triad.board
+  (:require-macros [cljs.core.async.macros :refer [go]])
   (:require [om.core :as om :include-macros true]
-            [sablono.core :as html :refer-macros [html]]))
+            [sablono.core :as html :refer-macros [html]]
+            [triple-triad.api :as api])
+  (:use [cljs.core.async :only [<!]]))
 
 
 (enable-console-print!)
 
 
-(defn board
+(def app-state
+  (atom {:cards []
+         :hands {:red []
+                 :blue []}}))
+
+
+(defn hand
+  [app owner {:keys [color]}]
+  (om/component
+   (html [:ul (for [card (get-in app [:hands color])]
+                [:li (:name card)])])))
+
+
+(defn grid
   [app]
   (om/component
-   (html [:h1 "Triple Triad Board"])))
+   (html [:div
+          [:div [:span] [:span] [:span]]
+          [:div [:span] [:span] [:span]]
+          [:div [:span] [:span] [:span]]])))
 
 
-(om/root board nil {:target (. js/document (getElementById "app"))})
+(defn board
+  [app]
+  (reify
+    om/IWillMount
+    (will-mount [_]
+      (go (let [cards (<! (api/fetch-cards))]
+            (om/update! app {:cards cards
+                             :hands {:red (take 5 (shuffle cards))
+                                     :blue (take 5 (shuffle cards))}}))))
+
+    om/IRender
+    (render [_]
+      (html [:div
+             [:h1 "Triple Triad Board"]
+             (om/build hand app {:opts {:color :red}})
+             (om/build grid app)
+             (om/build hand app {:opts {:color :blue}})]))))
+
+
+(om/root board app-state {:target (. js/document (getElementById "app"))})
