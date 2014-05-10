637a3b3e2f848539a1afddbd0dafa4525f654b92
Sidhant Godiwala <grinnbearit@liara.local>
2014-05-10 17:05:54 +0530

    refactor

--- src/client/triple_triad/api.cljs
+++ src/client/triple_triad/api.cljs
@@ -6,8 +6,47 @@
 
 
 (defn fetch-cards
+  "async: returns a list of cards"
   []
   (let [c (chan)]
     (go (let [res (<! (http/get "/cards.edn"))]
           (>! c (read-string (:body res)))))
     c))
+
+
+(defn- neighbours
+  [row col]
+  (for [[r c :as neighbour] [[(dec row) col :top :bottom]
+                             [(inc row) col :bottom :top]
+                             [row (dec col) :left :right]
+                             [row (inc col) :right :left]]
+        :when (and (<= 0 r 2) (<= 0 c 2))]
+    neighbour))
+
+
+(defn grid-step
+  "Returns the next grid state with the new card added"
+  [cards grid row col [player picked]]
+  (let [attacker (cards picked)]
+    (reduce (fn [g [r c atk blk]]
+              (let [[color idx] (get-in grid [r c])
+                    blocker (and idx (cards idx))]
+                (if (and blocker (not= color player)
+                         (< (blocker blk) (attacker atk)))
+                  (assoc-in g [r c] [player idx])
+                  g)))
+            (assoc-in grid [row col] [player picked])
+            (neighbours row col))))
+
+
+(defn winner
+  "Returns winnner if any, o/w returns nil"
+  [grid]
+  (let [values (apply concat grid)]
+    (when (not-any? nil? values)
+      (->> values
+           (map first)
+           (frequencies)
+           (sort-by second)
+           (last)
+           (first)))))
--- src/client/triple_triad/board.cljs
+++ src/client/triple_triad/board.cljs
@@ -22,66 +22,37 @@
                 [nil nil nil]]}))
 
 
+(defn card-single
+  [app owner {:keys [color idx]}]
+  (om/component
+   (html
+    [:div (cond-> {:class ["card" (name color)]}
+
+                  (= color (:player app))
+                  (assoc :on-click #(om/update! app :picked idx))
+
+                  (= idx (:picked app))
+                  (assoc :class ["card" "picked"]))
+
+     [:img {:src (get-in (:cards app) [idx :file])}]])))
+
+
 (defn card-hand
   [app owner {:keys [color]}]
   (om/component
-   (let [cards (:cards app)
-         hand (get-in app [:hand color])]
-     (html [:div.hand
-            (for [idx hand]
-              [:div (cond-> {:class ["card" (name color)]}
-
-                            (= color (:player app))
-                            (assoc :on-click #(om/update! app :picked idx))
-
-                            (= idx (:picked app))
-                            (assoc :class ["card" "picked"]))
-
-               [:img {:src (get-in cards [idx :file])}]])]))))
-
-
-(defn- neighbours
-  [row col]
-  (for [[r c :as neighbour] [[(dec row) col :top :bottom]
-                             [(inc row) col :bottom :top]
-                             [row (dec col) :left :right]
-                             [row (inc col) :right :left]]
-        :when (and (<= 0 r 2) (<= 0 c 2))]
-    neighbour))
-
-
-(defn- grid-step
-  [cards grid row col [player picked]]
-  (let [attacker (cards picked)]
-    (reduce (fn [g [r c atk blk]]
-              (let [[color idx] (get-in grid [r c])
-                    blocker (and idx (cards idx))]
-                (if (and blocker (not= color player)
-                         (< (blocker blk) (attacker atk)))
-                  (assoc-in g [r c] [player idx])
-                  g)))
-            (assoc-in grid [row col] [player picked])
-            (neighbours row col))))
-
-
-(defn- findout-winner
-  [grid]
-  (let [values (apply concat grid)]
-    (when (not-any? nil? values)
-      (->> values
-           (map first)
-           (frequencies)
-           (sort-by second)
-           (last)
-           (first)))))
+   (html
+    [:div.hand
+     (for [idx (get-in app [:hand color])]
+       (om/build card-single app {:opts {:color color
+                                         :idx idx}}))])))
 
 
 (defn- update-grid!
   [app e row col]
   (let [player (:player @app)
         picked (:picked @app)
-        grid (grid-step (:cards @app) (:grid @app) row col [player picked])
-        winner (findout-winner grid)]
+        grid (api/grid-step (:cards @app) (:grid @app) row col [player picked])
+        winner (api/winner grid)]
     (om/transact! app (fn [a]
                         (cond-> (-> (assoc a :grid grid)
                                     (assoc :player (case player :red :blue :red))
@@ -93,37 +64,76 @@
                                     (update-in [:score winner] inc)))))))
 
 
+(defn grid-cell
+  [app owner {:keys [row col]}]
+  (om/component
+   (html
+    (let [[color idx :as cell] (get-in (:grid app) [row col])]
+      (cond cell
+            [:div {:class ["card" (name color)]}
+             [:img {:src (get-in (:cards app) [idx :file])}]]
+
+            (:picked app)
+            [:div.card.neutral
+             {:on-click #(update-grid! app % row col)}]
+
+            :else
+            [:div.card.neutral])))))
+
+
+(defn grid-row
+  [app owner {:keys [row]}]
+  (om/component
+   (html
+    [:div (for [col (range 3)]
+            (om/build grid-cell app {:opts {:row row :col col}}))])))
+
+
 (defn card-grid
   [app]
   (om/component
-   (html [:div
-          (for [row (range 3)]
-            [:div
-             (for [col (range 3)
-                   :let [[color idx :as cell] (get-in (:grid app) [row col])]]
-               (cond  cell
-                      [:div {:class ["card" (name color)]}
-                       [:img {:src (get-in (:cards app) [idx :file])}]]
-
-                      (:picked app)
-                      [:div.card.neutral
-                       {:on-click #(update-grid! app % row col)}]
-
-                      :else
-                      [:div.card.neutral]))])])))
+   (html
+    [:div (for [row (range 3)]
+            (om/build grid-row app {:opts {:row row}}))])))
 
 
-(defn- reset-game!
+(defn score-board
+  [app]
+  (om/component
+   (html
+    [:div
+     [:span.red-text (str "red " (get-in app [:score :red]))]
+     [:span.blue-text (str "blue " (get-in app [:score :blue]))]])))
+
+
+(defn- reset-state!
   [app]
   (let [cards (:cards @app)
         idxs (take 10 (shuffle (range (count cards))))]
-    (om/transact! app (fn [a]
-                        (-> (assoc a :winner nil)
-                            (assoc-in [:hand :red] (take 5 idxs))
-                            (assoc-in [:hand :blue] (take 5 (drop 5 idxs)))
-                            (assoc :grid [[nil nil nil]
-                                          [nil nil nil]
-                                          [nil nil nil]]))))))
+    (om/transact! app #(-> (assoc % :winner nil)
+                           (assoc-in [:hand :red] (take 5 idxs))
+                           (assoc-in [:hand :blue] (take 5 (drop 5 idxs)))
+                           (assoc :grid [[nil nil nil]
+                                         [nil nil nil]
+                                         [nil nil nil]])))))
+
+
+(defn winner-header
+  [app]
+  (om/component
+   (html
+    [:div
+     [:span {:class (str (name (:winner app)) "-text")}
+      (str (name (:winner app)) " wins")]
+     [:button {:on-click #(reset-state! app)} "start over"]])))
+
+
+(defn player-header
+  [app]
+  (om/component
+   (html
+    [:span {:class (str (name (:player app)) "-text")}
+     (str (name (:player app)) "'s turn")])))
 
 
 (defn board
@@ -131,30 +141,24 @@
   (reify
     om/IWillMount
     (will-mount [_]
-      (go (let [cards (<! (api/fetch-cards))
-                idxs (take 10 (shuffle (range (count cards))))]
-            (om/transact! app (fn [a]
-                                (-> (assoc a :cards cards)
-                                    (assoc-in [:hand :red] (take 5 idxs))
-                                    (assoc-in [:hand :blue] (take 5 (drop 5 idxs)))))))))
+      (go (let [cards (<! (api/fetch-cards))]
+            (om/transact! app #(let [idxs (take 10 (shuffle (range (count cards))))]
+                                 (-> (assoc % :cards cards)
+                                     (assoc-in [:hand :red] (take 5 idxs))
+                                     (assoc-in [:hand :blue] (take 5 (drop 5 idxs)))))))))
 
     om/IRender
     (render [_]
-      (html [:div
-             [:h1 "Triple Triad Board"]
-             [:div
-              [:span.red-text (str "red " (get-in app [:score :red]))]
-              [:span.blue-text (str "blue " (get-in app [:score :blue]))]]
-             (if-let [winner (:winner app)]
-               [:div
-                [:span {:class (str (name (:winner app)) "-text")}
-                 (str (name (:winner app)) " wins")]
-                [:button {:on-click #(reset-game! app)} "start over"]]
-               [:span {:class (str (name (:player app)) "-text")}
-                (str (name (:player app)) "'s turn")])
-             (om/build card-hand app {:opts {:color :red}})
-             (om/build card-grid app)
-             (om/build card-hand app {:opts {:color :blue}})]))))
+      (html
+       [:div
+        [:h1 "Triple Triad Board"]
+        (om/build score-board app)
+        (if (:winner app)
+          (om/build winner-header app)
+          (om/build player-header app))
+        (om/build card-hand app {:opts {:color :red}})
+        (om/build card-grid app)
+        (om/build card-hand app {:opts {:color :blue}})]))))
 
 
 (om/root board app-state {:target (. js/document (getElementById "app"))})
